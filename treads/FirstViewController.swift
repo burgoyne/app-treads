4776a97d17ddc1d563635d87186962cdbd1eb895
Andre <aburgoyne@css.edu>
2018-10-04 13:15:15 -0600

    Initial Commit

---
+++ treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  treads
+//
+//  Created by Andre Burgoyne on 2018-10-04.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  treads
+//
+//  Created by Andre Burgoyne on 2018-10-04.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
